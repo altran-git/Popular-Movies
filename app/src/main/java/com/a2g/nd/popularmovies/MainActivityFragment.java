01961beb167231acd7d1459bcf45fff404f89ec6
Andy Tran <Andy Tran>
2016-05-10 20:58:37 -0700

    Implemented EndlessScrollListener to retrieve multiple pages from API

--- app/src/main/java/com/a2g/nd/popularmovies/MainActivityFragment.java
+++ app/src/main/java/com/a2g/nd/popularmovies/MainActivityFragment.java
@@ -1,6 +1,8 @@
 package com.a2g.nd.popularmovies;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -62,29 +64,44 @@ public class MainActivityFragment extends Fragment {
         // Inflate the fragment menu
         inflater.inflate(R.menu.menu_mainfragment, menu);
 
+
+        //Create and initialize the spinner object
         MenuItem item = menu.findItem(R.id.sort_spinner);
         Spinner sort_spinner = (Spinner) MenuItemCompat.getActionView(item);
-
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
                 R.array.sort_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
         sort_spinner.setAdapter(adapter);
 
+        //Listener for spinner object
         sort_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             public void onItemSelected(AdapterView<?> parent, View view,
                                        int position, long id) {
+
+                //clear adapter before resorting
+                movieAdapter.clear();
+
+                //Depending on which item in the spinner is selected, write the value to
+                //the shared preference key
+                SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+
                 switch (position){
                     case 0:
-                        movieAdapter.clear();
-                        new FetchMoviesTask().execute("popular","1");
+                        //Log.d("SPINNER", "Sort By Popular");
+                        editor.putString(getString(R.string.sort_by_key), "popular");
+                        editor.commit();
                         break;
                     case 1:
-                        movieAdapter.clear();
-                        new FetchMoviesTask().execute("top_rated","1");
+                        //Log.d("SPINNER", "Sort By Top Rated");
+                        editor.putString(getString(R.string.sort_by_key), "top_rated");
+                        editor.commit();
                         break;
                     default:
                 }
+
+                String sort_by = sharedPref.getString(getString(R.string.sort_by_key), getString(R.string.sort_by_default));
+                new FetchMoviesTask().execute(sort_by,"1");
             }
 
             @Override
@@ -96,6 +113,7 @@ public class MainActivityFragment extends Fragment {
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
+        //Save array list
         outState.putParcelableArrayList("movielist", movieArrayList);
     }
 
@@ -123,7 +141,45 @@ public class MainActivityFragment extends Fragment {
             }
         });
 
+        gridView.setOnScrollListener(new EndlessScrollListener() {
+            @Override
+            public boolean onLoadMore(int page, int totalItemsCount) {
+                // Triggered only when new data needs to be appended to the list
+                // Add whatever code is needed to append new items to your AdapterView
+
+                //read the shared preference key for total pages.  total pages was written during FetchMoviesTask
+                SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+                int total_pages = sharedPref.getInt(getString(R.string.total_pages_key), 1);
+
+                if(page <= total_pages ) {
+                    customLoadMoreDataFromApi(page);
+                    //Log.d("SCROLL TRUE", String.valueOf(page));
+                    return true;
+                }
+                else {
+                    //Log.d("SCROLL FALSE", String.valueOf(page));
+                    return false;
+                }
+                // or customLoadMoreDataFromApi(totalItemsCount);
+                 // ONLY if more data is actually being loaded; false otherwise.
+            }
+        });
+
         return rootView;
+    }
+
+    // Append more data into the adapter
+    public void customLoadMoreDataFromApi(int offset) {
+        // This method probably sends out a network request and appends new data items to your adapter.
+        // Use the offset value and add it as a parameter to your API request to retrieve paginated data.
+        // Deserialize API response and then construct new objects to append to the adapter
+
+        //Read the shared preference key for sorting
+        SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        String sort_by = sharedPref.getString(getString(R.string.sort_by_key), getString(R.string.sort_by_default));
+
+        //Log.d("SCROLL", "Sort By ---" + sort_by);
+        new FetchMoviesTask().execute(sort_by, String.valueOf(offset));
     }
 
     public class FetchMoviesTask extends AsyncTask<String,Void,Movie[]> {
@@ -138,15 +194,24 @@ public class MainActivityFragment extends Fragment {
             final String OVERVIEW = "overview";
             final String VOTE_AVG = "vote_average";
             final String REL_DATE = "release_date";
+            final String TOTAL_PAGES = "total_pages";
 
             JSONObject moviesJson = new JSONObject(moviesJsonStr);
             JSONArray movieArray = moviesJson.getJSONArray(MOVIE_ARRAY);
+
+            int total_pages = moviesJson.getInt(TOTAL_PAGES);
+
+            //write total pages to a shared preference key
+            SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putInt(getString(R.string.total_pages_key), total_pages);
+            editor.commit();
 
             //Create movie object
             Movie[] resultMovies = new Movie[movieArray.length()];
 
             for (int i = 0; i < movieArray.length() ; i++) {
-                // Get the JSON movie object
+                // Get the JSON movie objects
                 JSONObject movieObject = movieArray.getJSONObject(i);
                 String  movieImage = movieObject.getString(POSTER_PATH);
                 String  movieTitle = movieObject.getString(ORIG_TITLE);
@@ -256,6 +321,8 @@ public class MainActivityFragment extends Fragment {
 
         @Override
         protected void onPostExecute(Movie[] result) {
+            //Log.d(LOG_TAG, "onPost");
+
             //add data from server
             if (result != null) {
                 for(Movie movieObject : result){
